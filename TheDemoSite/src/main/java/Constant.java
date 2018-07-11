8af4d2a560264acd78949c57ccf74ececa024091
brieroth <brigitte.roth2018@outlook.com>
2018-07-11 15:34:05 +0100

    MercuryTours POM and Reporting

--- TheDemoSite/src/main/java/Constant.java
+++ TheDemoSite/src/main/java/Constant.java
@@ -1,10 +1,8 @@
 public class Constant {
 
-    public static final String Path_ExcelData =
-            "C:\\Users\\Admin\\Desktop\\AutomationReports\\";
+    public static final String Path_ExcelData =  "C:\\Users\\Admin\\Desktop\\AutomationReports\\";
 
-    public static final String File_ExcelData =
-            "DemoSiteDDT.xlsx";
+    public static final String File_ExcelData =   "DemoSiteDDT.xlsx";
 
     public static final String webHome = "http://thedemosite.co.uk";
     public static final String regPage = "http://thedemosite.co.uk/addauser.php";
